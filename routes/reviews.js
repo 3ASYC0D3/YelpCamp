b4611b8f5e9701c14d316fdbf3c6112a1288357d
Gulka_D <d.gulka4@gmail.com>
2021-08-16 12:33:56 +0200

    Moving middleware to separate file

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,18 +1,9 @@
 const express = require('express');
 const router = express.Router({ mergeParams: true });
 const catchAsync = require('../utils/catchAsync');
-const ExpressError = require('../utils/ExpressError');
 const Review = require('../models/review');
 const Campground = require('../models/campground');
-const { reviewSchema } = require('../schemas');
-
-const validateReview = (req, res, next) => {
-    const result = reviewSchema.validate(req.body);
-    if (result.error) {
-        throw new ExpressError(result.error.message, 400);
-    } else {
-        next();
-}};
+const { validateReview } = require('../middleware');
 
 router.post('/', validateReview, catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id);
