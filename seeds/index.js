e95f2956611fbe31f9fcdbf6323b1db1a29093c4
Gulka_D <d.gulka4@gmail.com>
2021-08-17 19:41:36 +0200

    Quick fix of seed to render any imgs on index page

--- seeds/index.js
+++ seeds/index.js
@@ -29,9 +29,67 @@ const seedDB = async () => {
             author: '61168c48446bd001e024ab68',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`, 
-            image: 'https://source.unsplash.com/collection/483251/960x540', 
             description: 'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi.',
-            price
+            price,
+            images: [
+                {
+                    url: 'https://res.cloudinary.com/darioyelpcamp/image/upload/v1629219781/YelpCamp/SKT14162_rggsrv.jpg',
+                    filename: 'YelpCamp/SKT14162_rggsrv'
+                },
+                {
+                    url: 'https://res.cloudinary.com/darioyelpcamp/image/upload/v1629219771/YelpCamp/SKT14088_xtr7o5.jpg',
+                    filename: 'YelpCamp/SKT14088_xtr7o5'
+                },
+                {
+                    url: 'https://res.cloudinary.com/darioyelpcamp/image/upload/v1629219770/YelpCamp/SKT13452_kwd2gp.jpg',
+                    filename: 'YelpCamp/SKT13452_kwd2gp'
+                },
+                // {
+                //     url: 'https://res.cloudinary.com/darioyelpcamp/image/upload/v1629215744/YelpCamp/mrsvzaxt0dx3e73rxnsx.jpg',
+                //     filename: 'YelpCamp/mrsvzaxt0dx3e73rxnsx'
+                // },
+                // {
+                //     url: 'https://res.cloudinary.com/darioyelpcamp/image/upload/v1629215482/YelpCamp/tpozsuexowmnllz5fscg.jpg',
+                //     filename: 'YelpCamp/tpozsuexowmnllz5fscg'
+                // },
+                // {
+                //     url: 'https://res.cloudinary.com/darioyelpcamp/image/upload/v1629215744/YelpCamp/mrsvzaxt0dx3e73rxnsx.jpg',
+                //     filename: 'YelpCamp/mrsvzaxt0dx3e73rxnsx'
+                // },
+                // {
+                //     url: 'https://res.cloudinary.com/darioyelpcamp/image/upload/v1629215482/YelpCamp/tpozsuexowmnllz5fscg.jpg',
+                //     filename: 'YelpCamp/tpozsuexowmnllz5fscg'
+                // },
+                // {
+                //     url: 'https://res.cloudinary.com/darioyelpcamp/image/upload/v1629215744/YelpCamp/mrsvzaxt0dx3e73rxnsx.jpg',
+                //     filename: 'YelpCamp/mrsvzaxt0dx3e73rxnsx'
+                // },
+                // {
+                //     url: 'https://res.cloudinary.com/darioyelpcamp/image/upload/v1629215482/YelpCamp/tpozsuexowmnllz5fscg.jpg',
+                //     filename: 'YelpCamp/tpozsuexowmnllz5fscg'
+                // },
+                // {
+                //     url: 'https://res.cloudinary.com/darioyelpcamp/image/upload/v1629215744/YelpCamp/mrsvzaxt0dx3e73rxnsx.jpg',
+                //     filename: 'YelpCamp/mrsvzaxt0dx3e73rxnsx'
+                // },
+                // {
+                //     url: 'https://res.cloudinary.com/darioyelpcamp/image/upload/v1629215482/YelpCamp/tpozsuexowmnllz5fscg.jpg',
+                //     filename: 'YelpCamp/tpozsuexowmnllz5fscg'
+                // },
+                // {
+                //     url: 'https://res.cloudinary.com/darioyelpcamp/image/upload/v1629215744/YelpCamp/mrsvzaxt0dx3e73rxnsx.jpg',
+                //     filename: 'YelpCamp/mrsvzaxt0dx3e73rxnsx'
+                // },
+                // {
+                //     url: 'https://res.cloudinary.com/darioyelpcamp/image/upload/v1629215482/YelpCamp/tpozsuexowmnllz5fscg.jpg',
+                //     filename: 'YelpCamp/tpozsuexowmnllz5fscg'
+                // },
+                // {
+                //     url: 'https://res.cloudinary.com/darioyelpcamp/image/upload/v1629215744/YelpCamp/mrsvzaxt0dx3e73rxnsx.jpg',
+                //     filename: 'YelpCamp/mrsvzaxt0dx3e73rxnsx'
+                // },
+        
+            ]
         });
         await camp.save();
     }
