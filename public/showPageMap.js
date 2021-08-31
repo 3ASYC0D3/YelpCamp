0d48fac7359ead3a7997058570e6e3f4861153c9
Gulka_D <d.gulka4@gmail.com>
2021-08-31 14:38:51 +0200

    Add nav controls. New map id

--- public/showPageMap.js
+++ public/showPageMap.js
@@ -2,11 +2,13 @@
 
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
-container: 'map', // container ID
+container: 'show-page-map', // container ID
 style: 'mapbox://styles/mapbox/streets-v11', // style URL
 center: campground.geometry.coordinates, // starting position [lng, lat]
 zoom: 9 // starting zoom
 });
+
+map.addControl(new mapboxgl.NavigationControl());
 
 const marker1 = new mapboxgl.Marker({color: 'orange'})
     .setLngLat(campground.geometry.coordinates)
